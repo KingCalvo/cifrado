2613a67a5e30aed3b8d81edf8823a148b5a31691
Jose-17-11 <ariashdez@outlook.com>
2025-02-19 08:35:17 -0600

    update style of document

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import ExampleComponent from "./components/ExampleComponent";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
-      <ExampleComponent />
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold" >Holi :)</h1>
     </div>
   );
 }
